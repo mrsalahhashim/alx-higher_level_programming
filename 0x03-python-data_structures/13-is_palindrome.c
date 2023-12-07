05c79ba9a60042b9a50741bdb639cba41c91d8ef
mrsalahhashim <mr.salahhashim@gmail.com>
2023-12-07 00:35:32 -0800

    0x03. Python - Data Structures: Lists, Tuples 04

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,54 +1,41 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
-*add_nodeint - adds a new node at the beginning of a listint_t list
-*@head: head of listint_t
-*@n: int to add in listint_t list
-*Return: address of the new element, or NULL if it failed
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
+ * palindrome - check if is palindrome with recursion
+ * @l: l
+ * @r: r
+ *
+ * Return: 1 palindrome, 0 not palindrome
+ */
+int palindrome(listint_t **l, listint_t *r)
 {
-	listint_t *new;
+	int response;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
-}
-/**
-*is_palindrome - identify if a syngle linked list is palindrome
-*@head: head of listint_t
-*Return: 1 if it is palindrome else 0
-*/
-int is_palindrome(listint_t **head)
-{
-	listint_t *head2 = *head;
-	listint_t *aux = NULL, *aux2 = NULL;
-
-	if (*head == NULL || head2->next == NULL)
-		return (1);
-	while (head2 != NULL)
+	if (r != NULL)
 	{
-		add_nodeint(&aux, head2->n);
-		head2 = head2->next;
-	}
-	aux2 = aux;
-	while (*head != NULL)
-	{
-		if ((*head)->n != aux2->n)
+		response = palindrome(l, r->next);
+		if (response != 0)
 		{
-			free_listint(aux);
-			return (0);
+			response = (r->n == (*l)->n);
+			*l = (*l)->next;
+			return (response);
 		}
-		*head = (*head)->next;
-		aux2 = aux2->next;
+		return (0);
+
 	}
-	free_listint(aux);
 	return (1);
 }
 
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: head of linked list
+ *
+ * Return: 1 palindrome, 0 not palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL)
+	{
+		return (0);
+	}
+	return (palindrome(head, *head));
+}
