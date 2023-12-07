0e5681341cb63edfc9e8390677a95f6cda7705ae
mrsalahhashim <mr.salahhashim@gmail.com>
2023-12-06 23:58:45 -0800

    0x03. Python - Data Structures: Lists, Tuples 01

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,29 @@
 #!/usr/bin/python3
-element_at = __import__('1-element_at').element_at
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to replace an element from a list at a specific position like in C
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+
+def replace_in_list(my_list, idx, element):
+    """Replaces an element of a list at a specific position
+
+    Args:
+        my_list: a list
+        idx: the index of item to replace
+        element: item to be substituted
+
+    Returns:
+        the edited list
+    """
+
+    # Check for negative and out of range index
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
