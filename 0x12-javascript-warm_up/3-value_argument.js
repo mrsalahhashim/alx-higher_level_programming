ca9c355a6c5d839d8dc557ff680ec5f455811a69
Salah Eldeen <mr.salahhashim@gmail.com>
2024-03-13 16:26:31 +0300

    commit 12

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-if (arg === undefined) {
+// prints the first argument passed to it
+
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(arg);
+  console.log(process.argv[2]);
 }
