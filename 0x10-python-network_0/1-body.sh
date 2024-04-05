e6b623c478af7974219efac79fbd6afa78b153b1
Nasser Tech <nassertechqtr@gmail.com>
2024-04-05 16:24:54 +0300

    commit3

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sX GET $1 -L
+#!/bin/bash                                                                   
+# a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response                                                  
+if [ "$(curl -sLI "$1" -X GET | grep "200 OK" | cut -d' ' -f2)" = '200' ]; then curl -sL "$1"; fi
