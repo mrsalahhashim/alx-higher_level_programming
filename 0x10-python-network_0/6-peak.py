edd7f427d71e979cd6ba7415e351d6c13cc6857e
Nasser Tech <nassertechqtr@gmail.com>
2024-04-05 16:29:34 +0300

    commitnetwork0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-""" Finds Peak values """
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-    """Find the peak"""
-    list_l = len(list_of_integers)
-    if list_l is 0:
+    """finds a peak in a list of unsorted integers"""
+    if (not list_of_integers):
         return None
-    peak = binary_search(list_of_integers, 0, list_l - 1)
-    return list_of_integers[peak]
 
+    if (len(list_of_integers) <= 2):
+        return max(list_of_integers)
 
-""" binary search algorithim """
+    peak = None
+    if (list_of_integers[0] >= list_of_integers[1]):
+        peak = list_of_integers[0]
+    if (list_of_integers[-1] >= list_of_integers[-2]):
+        peak = list_of_integers[-1]
+    if (peak):
+        return peak
 
-
-def binary_search(a, lo, hi):
-    """Recursive binary search of the peak"""
-    if lo >= hi:
-        return lo
-    mid = ((hi - lo) // 2) + lo
-    if a[mid] > a[mid + 1]:
-        return binary_search(a, lo, mid)
-    else:
-        return binary_search(a, mid + 1, hi)
+    i = 1
+    while (i < len(list_of_integers) - 1):
+        if (list_of_integers[i] >= list_of_integers[i + 1] and
+                list_of_integers[i] >= list_of_integers[i - 1]):
+            return list_of_integers[i]
+        else:
+            i += 1
+    return peak
