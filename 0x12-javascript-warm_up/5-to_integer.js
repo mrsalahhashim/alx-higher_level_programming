72db272abb771e301306b3052b04515a900a0cbf
mrsalahhashim <mr.salahhashim@gmail.com>
2024-04-26 05:53:11 -0700

    fix bugs

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-// prints two arguments passed to it, in the following format: “ is ”
-
-if (isNaN(process.argv[2])) {
+const arg = process.argv[2];
+if (isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + parseInt(process.argv[2]));
+  console.log('My number: ' + arg);
 }
