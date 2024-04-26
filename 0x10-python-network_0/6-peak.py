f498b76b3a2f130810606061a61fd0f26393dcc9
mrsalahhashim <mr.salahhashim@gmail.com>
2024-04-26 07:18:30 -0700

    peak bug fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-"""
-find the peak in an unordered list of
-integers
-"""
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-    """
-    find the peak in a list of intergers
-    Args:
-        list_of_integers(list) -> List of intergers
-    Return:
-        int(the peak integer(s))
-    """
-    if len(list_of_integers) == 0:
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
-    if len(list_of_integers) == 1:
+
+    size = len(list_of_integers)
+    if size == 1:
         return list_of_integers[0]
-    if len(list_of_integers) == 2:
+    elif size == 2:
         return max(list_of_integers)
 
-    mid = int(len(list_of_integers) / 2)
+    mid = int(size / 2)
     peak = list_of_integers[mid]
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
